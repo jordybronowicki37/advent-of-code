3d3d83ed3a7dfe80f4e0e186fffa2a57c7d6bca0
jordybronowicki37 <74408184+jordybronowicki37@users.noreply.github.com>
2022-12-22 19:48:06 +0100

    Made 2022 day 4

---
+++ 2022/day4/2.py
@@ -0,0 +1,24 @@
+file = open("input", "r")
+
+amount = 0
+
+for line in file:
+    line = line.replace("\n", "")
+    line = line.replace(",", "-")
+    split = line.split("-")
+
+    e1s1 = int(split[0])
+    e1s2 = int(split[1])
+    e2s1 = int(split[2])
+    e2s2 = int(split[3])
+
+    if e1s1 <= e2s2 <= e1s2:
+        amount += 1
+    elif e1s1 <= e2s1 <= e1s2:
+        amount += 1
+    elif e1s1 <= e2s1 and e1s2 >= e2s2:
+        amount += 1
+    elif e1s1 >= e2s1 and e1s2 <= e2s2:
+        amount += 1
+
+print(amount)
