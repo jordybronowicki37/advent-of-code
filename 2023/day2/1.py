a31d9057f59d3de70e09876e5e2566db23cb15c8
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-12-02 11:18:15 +0100

    Changed something in day 2 part 1

--- 2023/day2/1.py
+++ 2023/day2/1.py
@@ -13,22 +13,25 @@ for line in file:
 
     for cube_set in game_cubes_split[1].split("; "):
         cubes = cube_set.split(", ")
-        amount_red = 0
-        amount_green = 0
-        amount_blue = 0
 
         for cube in cubes:
             cube_amount_color = cube.split(" ")
             if cube_amount_color[1] == "red":
-                amount_red += int(cube_amount_color[0])
+                amount_red = int(cube_amount_color[0])
+                if amount_red > MAX_RED:
+                    condition_failed = True
+                    break
             elif cube_amount_color[1] == "green":
-                amount_green += int(cube_amount_color[0])
+                amount_green = int(cube_amount_color[0])
+                if amount_green > MAX_GREEN:
+                    condition_failed = True
+                    break
             elif cube_amount_color[1] == "blue":
-                amount_blue += int(cube_amount_color[0])
+                amount_blue = int(cube_amount_color[0])
+                if amount_blue > MAX_BLUE:
+                    condition_failed = True
+                    break
 
-        if amount_red > MAX_RED or amount_green > MAX_GREEN or amount_blue > MAX_BLUE:
-            condition_failed = True
-            break
     if not condition_failed:
         answer += game_id
 
