42c47a55bda70796c772e8f99441c83f8b0db3ec
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2023-12-02 11:05:49 +0100

    Fixed day 1 part 2

--- 2023/day1/2.py
+++ 2023/day1/2.py
@@ -2,56 +2,23 @@ file = open("input", "r")
 
 cvs = []
 answer = 0
+numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 
 def check_starts_with(input: str):
-    if input.startswith("one"):
-        return "1"
-    elif input.startswith("two"):
-        return "2"
-    elif input.startswith("three"):
-        return "3"
-    elif input.startswith("four"):
-        return "4"
-    elif input.startswith("five"):
-        return "5"
-    elif input.startswith("six"):
-        return "6"
-    elif input.startswith("seven"):
-        return "7"
-    elif input.startswith("eight"):
-        return "8"
-    elif input.startswith("nine"):
-        return "9"
-    elif input.startswith("zero"):
-        return "0"
-    else:
-        return ""
+    for i in range(len(numbers)):
+        num = numbers[i]
+        if input.startswith(num):
+            return str(i)
+    return ""
 
 
 def check_ends_with(input: str):
-    if input.endswith("one"):
-        return "1"
-    elif input.endswith("two"):
-        return "2"
-    elif input.endswith("three"):
-        return "3"
-    elif input.endswith("four"):
-        return "4"
-    elif input.endswith("five"):
-        return "5"
-    elif input.endswith("six"):
-        return "6"
-    elif input.endswith("seven"):
-        return "7"
-    elif input.endswith("eight"):
-        return "8"
-    elif input.endswith("nine"):
-        return "9"
-    elif input.endswith("zero"):
-        return "0"
-    else:
-        return ""
+    for i in range(len(numbers)):
+        num = numbers[i]
+        if input.endswith(num):
+            return str(i)
+    return ""
 
 
 for line in file:
