5cfbe0a88063f87e6de095f4d97cbed1c08f1107
jordy <74408184+jordybronowicki37@users.noreply.github.com>
2024-12-08 17:18:47 +0100

    Added 2024 day 7

---
+++ 2024/day7/2.py
@@ -0,0 +1,30 @@
+import itertools as it
+
+file = open("input", "r")
+
+answer = 0
+
+for line in file:
+    line = line.replace("\n", "").split(": ")
+    expect_result = int(line[0])
+    numbers = [int(n) for n in line[1].split(" ")]
+
+    operants_amount = len(numbers) - 1
+    operants_options = it.product(["+", "*", "|"], repeat=operants_amount)
+
+    for operants in operants_options:
+        actual_result = numbers[0]
+        for v in range(len(operants)):
+            op = operants[v]
+            if op == "+":
+                actual_result += numbers[v+1]
+            elif op == "*":
+                actual_result *= numbers[v+1]
+            elif op == "|":
+                actual_result = int(str(actual_result) + str(numbers[v+1]))
+        if expect_result == actual_result:
+            answer += actual_result
+
+            break
+
+print(answer)
